5ce38d8f1cf75c6e8306a38f71db23bfee4f9e22
Tos <tos311@gmail.com>
2023-10-19 22:28:44 +0200

    update lesson 17 18 19

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path")
 
-const postCSSPlugins = [require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+]
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
